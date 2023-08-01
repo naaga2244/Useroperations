75438af3252ad6268bf6856dedd551e9c117efed
NAAGA NARASIMHA <43115899+naaga2244@users.noreply.github.com>
2023-08-01 13:13:00 +0530

    Add files via upload

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,30 @@
+# file for signing, encodeing, decoding and returning jwt
+
+import time
+import jwt
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+# returns generated tokens
+def token_response(token: str):
+    return{
+        "access token" : token
+    }
+# used for signin
+def signJWT(userId: str):
+    payload = {
+        "userID" : userId,
+        "expiry" : time.time() + 600
+    }
+
+    token = jwt.encode(payload, JWT_SECRET, algorithm =JWT_ALGORITHM)
+    return token_response(token)
+
+def decodeJWT(token : str):
+    try:
+        decode_token = jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
+        return decode_token if decode_token['expires'] >= time.time() else None
+    except:
+        return {}
